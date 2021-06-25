aff827c1dcfcca43eac2530b1d9b0fde3c505d2d
Sam Rankin <srankin@riester.com>
2021-06-25 16:28:42 -0700

    Fixing vendor prefixes and phpstan

--- tests/phpstan/bootstrap.php
+++ tests/phpstan/bootstrap.php
@@ -9,7 +9,3 @@ define( 'RWP_PLUGIN_URI', plugin_dir_url( RWP_PLUGIN_ABSOLUTE ) );
 define( 'RWP_PLUGIN_VENDOR_PATH', RWP_PLUGIN_ROOT . 'includes/dependencies/' );
 define( 'RWP_PLUGIN_WP_VERSION', '5.6' );
 define( 'RWP_PLUGIN_PHP_VERSION', '7.0.0' );
-
-
-require_once dirname( __DIR__, 3 ) . '/advanced-custom-fields-pro/acf.php';
-require_once dirname( __DIR__, 3 ) . '/acf-extended/acf-extended.php';
