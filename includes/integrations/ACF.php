fc88c1c704f3beb129b842c9b640d71c4095c449
Sam Rankin <srankin@riester.com>
2021-06-29 10:48:02 -0700

    Updating save function to include all acf options pages

--- includes/integrations/ACF.php
+++ includes/integrations/ACF.php
@@ -23,37 +23,13 @@ class ACF extends Base {
 	public function initialize() {
 		parent::initialize();
 
-		\add_action( 'acf/init', array( $this, 'add_plugin_admin_menu' ) );
 		\add_action( 'acf/init', array( $this, 'setup_acf' ) );
 		\add_action( 'acfe/init', array( $this, 'init_acfe' ) );
-		\add_action( 'acfe/save_option/slug=' . $this->prefix( 'options', '-' ), array( $this, 'save_acf_options' ), 10, 2 );
+		\add_action( 'acfe/save_option', array( $this, 'save_acf_options_page' ), 10, 2 );
 		\add_action( 'acfe/save_term', array( $this, 'save_acf_term_fields' ), 10, 2 );
 		\add_action( 'acfe/save_post', array( $this, 'save_acf_post_fields' ), 10, 2 );
 		\add_action( 'acf/admin_enqueue_scripts', array( $this, 'enqueue_acf_assets' ), 999 );
 
-	}
-
-	/**
-	 * Register the administration menu for this plugin into the WordPress
-	 * Dashboard menu.
-	 *
-	 * @since 1.0.0
-	 * @return void
-	 */
-	public function add_plugin_admin_menu() {
-		// Check function exists.
-		if ( function_exists( 'acf_add_options_page' ) ) {
-
-			// Register options page.
-			acf_add_options_page(array(
-				'page_title' => __( 'RIESTERWP Core General Settings', 'rwp' ),
-				'menu_title' => __( 'RIESTERWP Core', 'rwp' ),
-				'menu_slug'  => $this->prefix( 'options', '-' ),
-				'capability' => $this->get_setting( 'capability' ),
-				'icon_url'   => $this->get_setting( 'icon' ),
-				'autoload'   => true,
-			));
-		}
 	}
 
 	/**
@@ -70,6 +46,19 @@ class ACF extends Base {
 
 		foreach ( glob( $path . '/*.*' ) as $file ) {
 			require_once $file;
+		}
+
+		if ( function_exists( 'acf_add_options_page' ) ) {
+
+			// Register options page.
+			acf_add_options_page(array(
+				'page_title' => __( 'RIESTERWP Core General Settings', 'rwp' ),
+				'menu_title' => __( 'RIESTERWP Core', 'rwp' ),
+				'menu_slug'  => $this->prefix( 'options', '-' ),
+				'capability' => $this->get_setting( 'capability' ),
+				'icon_url'   => $this->get_setting( 'icon' ),
+				'autoload'   => true,
+			));
 		}
 	}
 
@@ -102,7 +91,7 @@ class ACF extends Base {
 			$path = RWP_PLUGIN_ROOT . 'config/acf/';
 			\acfe_update_setting( 'php_save', $path );
 
-			$load_paths = \acf_get_setting( 'php_load' );
+			$load_paths = \acfe_get_setting( 'php_load' );
 			$load_paths[] = $path;
 			\acfe_update_setting( 'php_load', $load_paths );
 		}
@@ -122,9 +111,7 @@ class ACF extends Base {
 		$fields = \get_fields( $post_id );
 
 		$fields = self::sanitize_acf_array( $fields, $post_id ); // phpcs:ignore
-		if ( ! empty( $fields ) ) {
-			\update_post_meta( $object->ID, '_rwp_acf', $fields );
-		}
+		\update_post_meta( $object->ID, '_rwp_acf', $fields );
 	}
 
 	/**
@@ -141,9 +128,7 @@ class ACF extends Base {
 		$fields = \get_fields( $term_id );
 
 		$fields = self::sanitize_acf_array( $fields, $term_id ); // phpcs:ignore
-		if ( ! empty( $fields ) ) {
-			\update_term_meta( $object->term_id, '_rwp_acf', $fields );
-		}
+		\update_term_meta( $object->term_id, '_rwp_acf', $fields );
 	}
 
 	/**
@@ -155,14 +140,21 @@ class ACF extends Base {
      * @param array   $object   The ACF Options Page Settings array
      */
 
-	public function save_acf_options( $post_id, $object ) {
+	public function save_acf_options_page( $post_id, $object ) {
+
+		$option = data_get( $object, 'menu_slug', 'acf' );
 
 		$fields = \get_fields( $post_id );
 
 		$fields = self::sanitize_acf_array( $fields, $post_id ); // phpcs:ignore
-		if ( ! empty( $fields ) ) {
+
+		if ( $this->prefix( 'options', '-' ) === $option ) {
 			$this->update_option( 'options', $fields );
+        } else {
+			$option = rwp_change_case( $option, 'snake' );
+			update_option( $option, $fields );
 		}
+
 	}
 
 	/**
