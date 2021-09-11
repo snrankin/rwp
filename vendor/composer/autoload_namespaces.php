ee761ea4454609b83f614d3ae3b4badfff9c8086
Sam Rankin <srankin@riester.com>
2021-09-10 18:24:19 -0700

    Adding missing autoloader

--- vendor/composer/autoload_namespaces.php
+++ vendor/composer/autoload_namespaces.php
@@ -6,14 +6,4 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
-    'xrstf\\Composer52' => array($vendorDir . '/xrstf/composer-php52/lib'),
-    'tad_' => array($vendorDir . '/lucatume/args/src'),
-    'tad\\FunctionMocker' => array($vendorDir . '/lucatume/function-mocker/src'),
-    'cli' => array($vendorDir . '/wp-cli/php-cli-tools/lib'),
-    'WP_CLI\\' => array($vendorDir . '/wp-cli/wp-cli/php'),
-    'Requests' => array($vendorDir . '/rmccue/requests/library'),
-    'Mustache' => array($vendorDir . '/mustache/mustache/src'),
-    'Hautelook' => array($vendorDir . '/hautelook/phpass/src'),
-    'Behat\\Gherkin' => array($vendorDir . '/behat/gherkin/src'),
-    'Arg' => array($vendorDir . '/lucatume/args/src'),
 );
