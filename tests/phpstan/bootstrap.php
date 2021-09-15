fd81bdfc41cab8b75130cd82bef2c62dda7240a7
Sam Rankin <srankin@riester.com>
2021-09-15 13:23:26 -0700

    Updating ignore files

---
+++ tests/phpstan/bootstrap.php
@@ -0,0 +1,11 @@
+<?php
+
+define( 'RWP_PLUGIN_VERSION', '0.2.0' );
+define( 'RWP_PLUGIN_TEXTDOMAIN', 'rwp' );
+define( 'RWP_PLUGIN_NAME', 'RIESTERWP Core' );
+define( 'RWP_PLUGIN_ABSOLUTE', dirname( __DIR__, 2 ) . '/rwp.php' );
+define( 'RWP_PLUGIN_ROOT', plugin_dir_path( RWP_PLUGIN_ABSOLUTE ) );
+define( 'RWP_PLUGIN_URI', plugin_dir_url( RWP_PLUGIN_ABSOLUTE ) );
+define( 'RWP_PLUGIN_VENDOR_PATH', RWP_PLUGIN_ROOT . 'includes/dependencies/' );
+define( 'RWP_PLUGIN_WP_VERSION', '5.6' );
+define( 'RWP_PLUGIN_PHP_VERSION', '7.0.0' );
