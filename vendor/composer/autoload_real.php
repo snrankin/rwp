693520de7019774ba29dc1119398e4b0d9c7e269
Sam Rankin <srankin@riester.com>
2021-09-15 14:12:10 -0700

    Fixing composer

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -41,24 +41,6 @@ class ComposerAutoloaderInitb9a700ac9d58e3a3255ca579cae564a9
         $loader->setClassMapAuthoritative(true);
         $loader->register(true);
 
-        if ($useStaticLoader) {
-            $includeFiles = Composer\Autoload\ComposerStaticInitb9a700ac9d58e3a3255ca579cae564a9::$files;
-        } else {
-            $includeFiles = require __DIR__ . '/autoload_files.php';
-        }
-        foreach ($includeFiles as $fileIdentifier => $file) {
-            composerRequireb9a700ac9d58e3a3255ca579cae564a9($fileIdentifier, $file);
-        }
-
         return $loader;
-    }
-}
-
-function composerRequireb9a700ac9d58e3a3255ca579cae564a9($fileIdentifier, $file)
-{
-    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
-        require $file;
-
-        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
