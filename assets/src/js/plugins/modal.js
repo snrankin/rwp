3a6b5ea3667eabcd3e2105b96360c1bef5c8dac5
Sam Rankin <srankin@riester.com>
2021-09-22 16:18:47 -0700

    Removing Bootstrap icon from fancybox plugin

--- assets/src/js/plugins/modal.js
+++ assets/src/js/plugins/modal.js
@@ -16,9 +16,6 @@ import { assign, has, isUndefined, defaultsDeep, isNil } from 'lodash';
 
 Fancybox.bind('[data-fancybox]', {
 	template: {
-		// Close button icon
-		closeButton:
-			'<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16"><path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/></svg>',
 		// Loading indicator icon
 		spinner:
 			'<span class="btn-icon has-spinner"><span class="spinner-border" role="status"><span class="visually-hidden">Loading...</span></span></span>',
