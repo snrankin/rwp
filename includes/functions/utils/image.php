4f656013f983331a0e503a1f43f0f4fcbe105a35
Sam Rankin <srankin@riester.com>
2022-01-27 10:27:36 -0700

    Adding image sttribute functions

--- includes/functions/utils/image.php
+++ includes/functions/utils/image.php
@@ -50,6 +50,222 @@ function rwp_registered_image_sizes() {
     return $sizes;
 }
 
+function rwp_media_path( $image = null ) {
+
+	if ( empty( $image ) ) {
+		return false;
+    }
+
+	if ( is_string( $image ) ) {
+		if ( rwp_is_url( $image ) && ! rwp_is_outbound_link( $image ) ) {
+			$image = attachment_url_to_postid( $image );
+		} elseif ( is_numeric( $image ) ) {
+			$image = intval( $image );
+		}
+	}
+
+	if ( $image instanceof \WP_Post ) {
+		$image = $image->ID;
+	}
+
+	if ( ! wp_attachment_is_image( $image ) ) {
+		return false;
+	}
+
+	$uploads_path = wp_get_upload_dir();
+	$uploads_dir  = trailingslashit( $uploads_path['basedir'] );
+	$uploads_uri  = trailingslashit( rwp_relative_url( $uploads_path['baseurl'] ) );
+
+	$img_dir = wp_get_original_image_path( $image );
+	$img_folder = trailingslashit( pathinfo( $img_dir, PATHINFO_DIRNAME ) );
+	$img_folder = str_replace( $uploads_dir, '', $img_folder );
+	$ext = '.' . pathinfo( $img_dir, PATHINFO_EXTENSION );
+	$name = wp_basename( $img_dir, $ext );
+	$size = getimagesize( $img_dir );
+
+	$info = [
+		'uploads_uri' => $uploads_uri,
+		'uploads_dir' => $uploads_dir,
+		'folder'      => $img_folder,
+		'image_uri'   => trailingslashit( dirname( rwp_relative_url( wp_get_original_image_url( $image ) ) ) ),
+		'image_dir'   => trailingslashit( dirname( $img_dir ) ),
+		'filename'    => $name,
+		'ext'         => $ext,
+	];
+
+	if ( $size ) {
+		$info['mime'] = $size['mime'];
+		$info['width'] = $size[0];
+		$info['height'] = $size[1];
+	}
+
+	return $info;
+}
+
+/**
+ * Get all the registered image sizes along with their dimensions
+ *
+ * @param string|int|\WP_Post $image The id of the image;
+ *
+ * @link http://core.trac.wordpress.org/ticket/18947 Reference ticket
+ *
+ * @return array $image_sizes The image sizes
+ */
+function rwp_image_sizes( $image ) {
+
+	if ( is_string( $image ) ) {
+		if ( rwp_is_url( $image ) && ! rwp_is_outbound_link( $image ) ) {
+			$image = attachment_url_to_postid( $image );
+		} elseif ( is_numeric( $image ) ) {
+			$image = intval( $image );
+		}
+	}
+
+	if ( $image instanceof \WP_Post ) {
+		$image = $image->ID;
+	}
+
+	if ( ! wp_attachment_is_image( $image ) ) {
+		return false;
+	}
+
+	$type = wp_get_original_image_url( $image );
+
+	if ( $type ) {
+		$type = pathinfo( $type, PATHINFO_EXTENSION );
+	}
+
+	$sizes = [];
+
+	$media_path = rwp_media_path( $image );
+
+	if ( $media_path && rwp_array_has( 'mime', $media_path ) ) {
+
+		$mime = $media_path['mime'];
+	}
+
+	if ( 'svg' !== $type ) {
+		$sizes = rwp_registered_image_sizes();
+
+		$sizes = rwp_collection( $sizes );
+
+		if ( $sizes->isNotEmpty() ) {
+			$sizes->transform(function ( $item, $size ) use ( $image, $media_path, $mime ) {
+
+				$img_src = image_get_intermediate_size( $image, $size );
+
+				if ( $img_src ) {
+					if ( rwp_array_has( 'url', $img_src ) ) {
+						$img_src['url'] = rwp_relative_url( $img_src['url'] );
+					}
+					return $img_src;
+				}
+
+				$ext = $media_path['ext'];
+				$name = $media_path['filename'];
+
+				$file_name = $name;
+				if ( 'full' !== $size ) {
+					if ( rwp_array_has( 'width', $item ) && rwp_array_has( 'height', $item ) ) {
+						$file_name .= '-' . $item['width'] . 'x' . $item['height'];
+					}
+				} else {
+					$item['width']  = $media_path['width'];
+					$item['height'] = $media_path['height'];
+				}
+
+				$file_name .= $ext;
+
+				$file_path = wp_normalize_path( $media_path['image_dir'] . $file_name );
+				$file_uri = $media_path['image_uri'] . $file_name;
+				if ( file_exists( $file_path ) ) {
+					$item['file']      = $file_name;
+					$item['path']      = $media_path['folder'] . $file_name;
+					$item['url']       = $file_uri;
+					$item['mime-type'] = $mime;
+				}
+
+				return $item;
+			});
+		}
+		$sizes = $sizes->reject(function ( $item ) {
+
+			return ! rwp_array_has( 'file', $item );
+		});
+
+		$sizes = $sizes->sortBy( 'width' );
+
+		$sizes = $sizes->all();
+		return $sizes;
+	} else {
+		return false;
+	}
+}
+
+// @ Get Srcset
+function rwp_get_srcset( $id, $size = 'full' ) {
+	$sizes = rwp_image_sizes( $id );
+
+	$type = wp_get_original_image_url( $id );
+
+	if ( $type ) {
+		$type = pathinfo( $type, PATHINFO_EXTENSION );
+	}
+
+	if ( 'svg' !== $type ) {
+		if ( $sizes ) {
+
+			$sizes = rwp_collection( $sizes );
+			if ( 'full' !== $size ) {
+
+				$size_index = $sizes->keys()->search( $size );
+				if ( false !== $size_index ) {
+
+					$size_keys = $sizes->keys()->takeUntil(function ( $item, $key ) use ( $size_index ) {
+						return $key > $size_index;
+					});
+
+					$sizes = $sizes->only( $size_keys->all() );
+				}
+			}
+
+			$sources = [];
+			$srcset = [];
+			if ( $sizes->isNotEmpty() && $sizes->count() > 1 ) {
+				// The 'src' image has to be the first in the 'srcset', because
+				// of a bug in iOS8 but NOT the first <source>
+				$last = $sizes->last();
+				$last_size = $sizes->keys()->last();
+
+				$srcset[ $last_size ] = sprintf( '%1$s %2$dw %3$dh', $last['url'], $last['width'], $last['height'] );
+				$sources['sizes'][ $size ] = $last;
+
+				foreach ( $sizes->all() as $k => $v ) {
+					if ( $last_size !== $k ) { // So the last size isn't added twice
+						$srcset[ $k ] = sprintf( '%1$s %2$dw %3$dh', $v['url'], $v['width'], $v['height'] );
+					}
+
+					$sources['sizes'][ $k ] = $v;
+				}
+				$srcset = implode( ', ', $srcset );
+				$sources['srcset'] = $srcset;
+			}
+
+			if ( ! empty( $sources ) ) {
+				return $sources;
+			}
+		}
+	}
+	return false;
+}
+
+// @ Has Dimensions
+function rwp_image_has_dimensions( $width = null, $height = null ) {
+	if ( ( ! empty( $width ) && intval( $width ) != 0 ) && ( ! empty( $height ) && intval( $height ) != 0 ) ) {
+		return true;
+    }
+}
+
 /**
  * Check if the variable is an image url, id or instance of WP_Post
  *
@@ -353,4 +569,148 @@ function rwp_get_featured_image( $post = null, $size = 'full', $args = [], $html
 	} else {
 		return false;
 	}
+}
+
+/**
+ * Get dimensions of svg from viewbox attributes
+ *
+ * @param string $file
+ * @return array|false
+ */
+function rwp_get_svg_dimensions( $file ) {
+	$svg_file = simplexml_load_file( $file );
+	if ( $svg_file ) {
+		$svg_file = rwp_xml_to_array( $svg_file );
+		$view_box = null;
+
+		if ( rwp_array_has( 'viewBox', $svg_file['atts'] ) ) {
+			$view_box = $svg_file['atts']['viewBox'];
+			$view_box = explode( ' ', $view_box );
+		}
+
+		if ( rwp_array_has( 'width', $svg_file['atts'] ) ) {
+			$width = $svg_file['atts']['width'];
+		} elseif ( ! empty( $view_box ) ) {
+			$width = floatval( $view_box[2] );
+		}
+
+		if ( rwp_array_has( 'height', $svg_file['atts'] ) ) {
+			$height = $svg_file['atts']['height'];
+		} elseif ( ! empty( $view_box ) ) {
+			$height = floatval( $view_box[3] );
+		}
+		return array(
+			'width' => $width,
+			'height' => $height,
+		);
+	} else {
+		return false;
+	}
+}
+
+/**
+ * Filters the list of attachment image attributes.
+ *
+ * @since 2.8.0
+ *
+ * @param array        $attr       Array of attribute values for the image markup, keyed by attribute name.
+ *                                 @see wp_get_attachment_image().
+ * @param int|string|\WP_Post      $attachment Image attachment post.
+ * @param string|array $size       Requested size. Image size or array of width and height values
+ *                                 (in that order). Default 'thumbnail'.
+ */
+function rwp_image_attrs( $attr = array(), $attachment = 0, $size = 'full' ) {
+	if ( is_string( $attachment ) && rwp_is_url( $attachment ) && ! rwp_is_outbound_link( $attachment ) ) {
+		$attachment = attachment_url_to_postid( $attachment );
+	}
+
+	$attachment = rwp_post_object( $attachment );
+	$attachment_id = data_get( $attachment, 'id', 0 );
+	$attachment_object = data_get( $attachment, 'object' );
+
+	if ( ! empty( $attr ) ) {
+		$attr = rwp_prepare_args( $attr );
+	}
+
+	$url = '';
+	$sizes = array();
+
+	if ( wp_attachment_is_image( $attachment_id ) ) {
+		$url   = rwp_relative_url( wp_get_attachment_image_url( $attachment_id, $size ) );
+		$sizes = rwp_image_sizes( $attachment_object, $size );
+	} else {
+		if ( rwp_array_has( 'src', $attr ) ) {
+			$url = $attr['src'];
+		} elseif ( rwp_array_has( 'data-src', $attr ) ) {
+			$url = $attr['data-src'];
+		}
+	}
+
+	$type = pathinfo( $url, PATHINFO_EXTENSION );
+
+	if ( rwp_array_has( 'style', $attr ) ) {
+		$attr['style'] = rwp_parse_styles( $attr['style'] );
+	}
+
+	$width  = data_get( $attr, 'width', null );
+	$height = data_get( $attr, 'height', null );
+
+	if ( ! rwp_image_has_dimensions( $width, $height ) ) {
+		if ( 'svg' === $type ) {
+			$file = wp_get_original_image_path( $attachment_id );
+			$svg_dimensions = rwp_get_svg_dimensions( $file );
+			if ( $svg_dimensions ) {
+				list($width, $height) = $svg_dimensions;
+			}
+		} else {
+			if ( rwp_array_has( $size, $sizes ) ) {
+				if ( rwp_array_has( 'width', $sizes[ $size ] ) ) {
+					$width = $sizes[ $size ]['width'];
+				}
+				if ( rwp_array_has( 'height', $sizes[ $size ] ) ) {
+					$height = $sizes[ $size ]['height'];
+				}
+			}
+		}
+	}
+
+	if ( rwp_image_has_dimensions( $width, $height ) ) {
+		if ( ! rwp_array_has( 'width', $attr ) ) {
+			$attr['width'] = $width;
+		}
+		if ( ! rwp_array_has( 'height', $attr ) ) {
+			$attr['height'] = $height;
+		}
+	}
+
+	if ( ! rwp_array_has( 'srcset', $attr ) ) {
+
+		$srcset = rwp_get_srcset( $attachment_id, $size );
+
+		if ( $srcset ) {
+			$attr['srcset'] = $srcset['srcset'];
+		}
+	}
+
+	if ( ! rwp_array_has( 'alt', $attr ) ) {
+
+		$image_alt = get_post_meta( $attachment_id, '_wp_attachment_image_alt', true );
+
+		if ( empty( $image_alt ) && $attachment_object instanceof \WP_Post ) {
+			$image_alt = $attachment_object->post_title;
+		}
+		if ( empty( $image_alt ) ) {
+			$image_alt = wp_get_attachment_caption( $attachment_id );
+		}
+
+		if ( empty( $image_alt ) && $attachment_object instanceof \WP_Post ) {
+			$image_alt = $attachment_object->post_excerpt;
+		}
+	}
+
+	if ( rwp_array_has( 'style', $attr ) && is_array( $attr['style'] ) ) {
+		$attr['style'] = rwp_output_styles( $attr['style'] );
+	}
+
+	return $attr;
 }
