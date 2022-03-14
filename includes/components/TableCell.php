d3909819a6193e913e5da1067975d83cca6d2c90
Sam Rankin <srankin@riester.com>
2022-03-13 19:16:01 -0700

    Adding table component

---
+++ includes/components/TableCell.php
@@ -0,0 +1,23 @@
+<?php
+/** ============================================================================
+ * TableCell
+ *
+ * @package   RWP\/includes/components/Column.php
+ * @since     1.0.0
+ * @author    RIESTER <wordpress@riester.com>
+ * @copyright 2020 - 2021 RIESTER Advertising Agency
+ * @license   GPL-2.0+
+ * ========================================================================== */
+
+namespace RWP\Components;
+
+use RWP\Components\Collection;
+
+class TableCell extends Element {
+
+	/**
+	 * @var string $tag The html element tag
+	 */
+	public $tag = 'td';
+
+}
