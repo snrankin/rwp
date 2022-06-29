f1e2772d387391fc3486fb225251998e153604ed
Sam Rankin <srankin@riester.com>
2022-06-29 11:26:36 -0700

    Fixing lazysizes sizes issue

--- src/js/vendor/lazysizes.js
+++ src/js/vendor/lazysizes.js
@@ -7,9 +7,9 @@
  * ========================================================================== */
 
 import(/* webpackMode: "eager" */ 'lazysizes');
-import(/* webpackMode: "eager" */ 'lazysizes/plugins/parent-fit/ls.parent-fit');
-import(/* webpackMode: "eager" */ 'lazysizes/plugins/aspectratio/ls.aspectratio');
 import(/* webpackMode: "eager" */ 'lazysizes/plugins/respimg/ls.respimg');
-import(/* webpackMode: "eager" */ 'lazysizes/plugins/native-loading/ls.native-loading');
+import(/* webpackMode: "eager" */ 'lazysizes/plugins/parent-fit/ls.parent-fit');
+import(/* webpackMode: "eager" */ 'lazysizes/plugins/object-fit/ls.object-fit');
+import(/* webpackMode: "eager" */ 'lazysizes/plugins/aspectratio/ls.aspectratio');
 import(/* webpackMode: "eager" */ 'lazysizes/plugins/print/ls.print');
 import(/* webpackMode: "eager" */ 'lazysizes/plugins/video-embed/ls.video-embed');
