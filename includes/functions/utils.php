d7cd37236429440a04ed342ddcf20ca546abb65c
Sam Rankin <srankin@riester.com>
2022-07-22 18:31:50 -0700

    style: fixing some style errors from phpcs

--- includes/functions/utils.php
+++ includes/functions/utils.php
@@ -242,9 +242,9 @@ function rwp_hex_to_rgb( $color ) {
 	if ( '#' === $color[0] ) {
 		$color = substr( $color, 1 );
 	}
-	if ( strlen( $color ) == 6 ) {
+	if ( strlen( $color ) === 6 ) {
 		list($r, $g, $b) = array( $color[0] . $color[1], $color[2] . $color[3], $color[4] . $color[5] );
-	} elseif ( strlen( $color ) == 3 ) {
+	} elseif ( strlen( $color ) === 3 ) {
 		list($r, $g, $b) = array( $color[0] . $color[0], $color[1] . $color[1], $color[2] . $color[2] );
 	} else {
 		return false;
